4efa19a10523e7b24715948f4dfa361dcbae14ec
jakubkoziel <jakub.koziel@interia.pl>
2023-12-20 22:09:54 +0100

    Add streamlit basis

---
+++ streamlit_app/app.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import pandas as pd
+
+# st.write("Here's our first attempt at using data to create a table:")
+# st.write(pd.DataFrame({
+#     'first column': [1, 2, 3, 4],
+#     'second column': [10, 20, 30, 40]
+# }))
+#
+
+import plotly.graph_objects as go
+
+categories = ['processing cost','mechanical properties','chemical stability',
+              'thermal stability', 'device integration']
+
+fig = go.Figure()
+
+fig.add_trace(go.Scatterpolar(
+      r=[1, 5, 2, 2, 3],
+      theta=categories,
+      fill='toself',
+      name='Product A'
+))
+fig.add_trace(go.Scatterpolar(
+      r=[4, 3, 2.5, 1, 2],
+      theta=categories,
+      fill='toself',
+      name='Product B'
+))
+
+fig.update_layout(
+  polar=dict(
+    radialaxis=dict(
+      visible=True,
+      range=[0, 5]
+    )),
+  showlegend=False
+)
+
+fig.show()
