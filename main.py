c3d80fef197b2204ab80fc96f2000183e3d25e10
liskuba <01151424@pw.edu.pl>
2023-12-22 02:30:28 +0100

    add printing themes and their relations in main.py

--- main.py
+++ main.py
@@ -3,23 +3,29 @@ import os
 import pandas as pd
 from SPARQLWrapper import JSON, SPARQLWrapper
 
+EUROVOC_JENA = "eurovoc"
+PREFIXES = """
+    PREFIX dcat:    <http://www.w3.org/ns/dcat#>
+    PREFIX dct:     <http://purl.org/dc/terms/>
+    PREFIX eurovoc: <http://eurovoc.europa.eu/>
+    PREFIX foaf:    <http://xmlns.com/foaf/0.1/>
+    PREFIX odp:     <http://data.europa.eu/euodp/ontologies/ec-odp#>
+    PREFIX rb:      <https://w3id.org/riverbench/schema/metadata#>
+    PREFIX rbdoc:   <https://w3id.org/riverbench/schema/documentation#>
+    PREFIX rdfs:    <http://www.w3.org/2000/01/rdf-schema#>
+    PREFIX skos:    <http://www.w3.org/2004/02/skos/core#>
+    PREFIX spdx:    <http://spdx.org/rdf/terms#>
+    PREFIX stax:    <https://w3id.org/stax/ontology#>
+    PREFIX xsd:     <http://www.w3.org/2001/XMLSchema#>
+"""
+
 
 def get_data(dataset_name, statistics_type):
     url = f"http://localhost:3030/{dataset_name}/query"
     sparql = SPARQLWrapper(url)
     sparql.setReturnFormat(JSON)
     query = f"""
-        PREFIX dcat:    <http://www.w3.org/ns/dcat#>
-        PREFIX dcterms: <http://purl.org/dc/terms/>
-        PREFIX eurovoc: <http://eurovoc.europa.eu/>
-        PREFIX foaf:    <http://xmlns.com/foaf/0.1/>
-        PREFIX rb:      <https://w3id.org/riverbench/schema/metadata#>
-        PREFIX rbdoc:   <https://w3id.org/riverbench/schema/documentation#>
-        PREFIX rdfs:    <http://www.w3.org/2000/01/rdf-schema#>
-        PREFIX spdx:    <http://spdx.org/rdf/terms#>
-        PREFIX stax:    <https://w3id.org/stax/ontology#>
-        PREFIX xsd:     <http://www.w3.org/2001/XMLSchema#>
-        
+        {PREFIXES}
         SELECT *
         WHERE {'{'}
           <https://w3id.org/riverbench/datasets/{dataset_name}/{statistics_type}> rb:hasStatistics ?stats .
@@ -56,6 +62,43 @@ def get_data(dataset_name, statistics_type):
     return pd.DataFrame(data)
 
 
+def get_eurovoc_themes(dataset_name, predicate="skos:narrower+"):
+    url_dataset = f"http://localhost:3030/{dataset_name}/query"
+    sparql_dataset = SPARQLWrapper(url_dataset)
+    sparql_dataset.setReturnFormat(JSON)
+    query_dataset = f"""
+        {PREFIXES}
+        SELECT DISTINCT ?theme
+        WHERE {'{'}
+            <https://w3id.org/riverbench/datasets/{dataset_name}/dev> dcat:theme ?theme .
+        {'}'}
+    """
+    sparql_dataset.setQuery(query_dataset)
+    results = sparql_dataset.query().convert()
+    themes = []
+    for result in results["results"]["bindings"]:
+        themes.append(result["theme"]["value"].split("/")[-1])
+    url_eurovoc = f"http://localhost:3030/{EUROVOC_JENA}/query"
+    sparql_eurovoc = SPARQLWrapper(url_eurovoc)
+    sparql_eurovoc.setReturnFormat(JSON)
+    for theme in themes:
+        query_eurovoc = f"""
+            {PREFIXES}
+            SELECT DISTINCT ?theme ?label
+            WHERE {'{'}
+                eurovoc:{theme} {predicate} ?theme .
+                ?theme skos:prefLabel ?label .
+                FILTER(LANG(?label) = "en")
+            {'}'}
+        """
+        sparql_eurovoc.setQuery(query_eurovoc)
+        results = sparql_eurovoc.query().convert()
+        themes_predicate = []
+        for result in results["results"]["bindings"]:
+            themes_predicate.append(result["theme"]["value"].split("/")[-1])
+        print(theme, themes_predicate)
+
+
 def main():
     statistics = "dev#statistics-full"
     # statistics = "dev#statistics-100k"
@@ -80,6 +123,10 @@ def main():
     for dataset in datasets[1:]:
         dataframe = pd.concat([dataframe, get_data(dataset, statistics)])
     dataframe.to_csv(f"{dir_to_save}/all_datasets.csv", index=False)
+    for dataset in datasets:
+        print(dataset)
+        get_eurovoc_themes(dataset, "skos:related+")
+        print()
 
 
 if __name__ == "__main__":
